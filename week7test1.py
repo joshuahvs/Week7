20eafe9c0acfb3e446f1c69690a20085f9d83fb2
vito1012buss <118058551+vito1012buss@users.noreply.github.com>
2023-10-10 12:41:24 +0000

    week 7

--- week7test1.py
+++ week7test1.py
@@ -1,10 +1,15 @@
-def reverse(list):
-    new_list = []
-    for i in range(1, len(list)+1):
-        new_List += [(list[-i])]
-        return new_list
+# def reverse(list):
+#     new_list = []
+#     for i in range(1, len(list)+1):
+#         new_List += [(list[-i])]
+#     return new_list
 
-print(output)
+# print(reverse([1,2,3]))
 
-reverse([1,2,3])
-#whattt
+def reverse(a:list):
+  new_list = []
+  for i in range(1, len(a)+1):
+    new_list += [(a[-i])]
+  return new_list
+
+print(reverse([1,2,3]))
